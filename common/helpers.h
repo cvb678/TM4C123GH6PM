c32da6cba8066e5ac367fc0cb5bb63b83754c3ac
Michal Kozicki <mkmichalk04@gmail.com>
2020-10-19 20:28:28 +0200

    Update example code to use sysctl and pins_config

--- common/helpers.h
+++ common/helpers.h
@@ -7,3 +7,12 @@ typedef long long uint64_t;
 typedef long int32_t;
 typedef short int16_t;
 typedef char int8_t;
+
+#define PIN0 0x01
+#define PIN1 0x02
+#define PIN2 0x04
+#define PIN3 0x08
+#define PIN4 0x10
+#define PIN5 0x20
+#define PIN6 0x40
+#define PIN7 0x80
--- example1/main.c
+++ example1/main.c
@@ -3,16 +3,19 @@
 int main (void)
 {
     gpio_port_s portA;
+    gpio_pin_config portA_pins_cfg;
     portA.port_num = GPIO_PORT_A;
-    portA.pins_used_mask = 0x20; //pin 5
-    portA.port_control_mask = 0x00F00000; //
+    portA.pins_used_mask = PIN5;
+    portA.port_control_mask = 0x00F00000;
     portA.port_control = GPIO_PORT_CONTROL_SET;
-    portA.pins_analog_function = GPIO_ANALOG_MODE_DISABLED;
-    portA.pins_direction = GPIO_DIR_INPUT;
-    portA.pins_alternate_control = GPIO_REGISTER_CTRL;
-    portA.pins_digital_enable = GPIO_DIGITAL_ENABLE;
+    portA_pins.analog_function = GPIO_ANALOG_MODE_DISABLED;
+    portA_pins.direction = GPIO_DIR_INPUT;
+    portA_pins.alternate_control = GPIO_REGISTER_CTRL;
+    portA_pins.digital_enable = GPIO_DIGITAL_ENABLE;
 
-    gpio_port_init(&portA);
+    gpio_port_init(&portA, &portA_pins_cfg);
+    gpio_configure_pins(&portA);
+
     while(1)
     {
         /**/
